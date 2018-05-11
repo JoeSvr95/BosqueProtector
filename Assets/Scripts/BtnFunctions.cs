9ea8883385fb13aafc7de83d0f0c4577ce361784
Joe Saverio <joesaverio@hotmail.com>
2018-05-11 13:31:00 -0500

    Mapita

---
+++ Assets/Scripts/BtnFunctions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BtnFunctions : MonoBehaviour {
+
+	public int station;
+
+	public void DisplayText (){
+		GetComponentInChildren<Text> ().text = "Estacion " + station;
+	}
+
+	public void HideText (){
+		GetComponentInChildren<Text> ().text = "";
+	}
+
+	public void GoToVisualizer() {
+		Debug.Log("Visualizer estacion " + station);
+	}
+}
